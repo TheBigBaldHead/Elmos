626d75cf5065a6c0537e4a4bd8921a1837bafa5c
Amin <afiroozi83@gmail.com>
2022-09-27 21:49:51 +0330

    Update hoop.py

--- hoop.py
+++ hoop.py
@@ -2,7 +2,7 @@ import time
 print("Let's play hoop\nYou have 5 seconds for each turn! - Type * instead of hoop!")
 hoop = int(input("\nPlease insert hoop cycle length (a number): "))
 
-def computer():
+def is_computer():
     if number%2 == 1:
         return True
     else:
@@ -14,20 +14,30 @@ def is_hoop():
     else:
         return False
 
-def True_answer():
-    if not is_hoop():
-        if int(answer) == number:
-            return True
+def True_answer(answer):
+    try:
+        if is_hoop():
+            if answer == "*":
+                return True
+            else:
+                return False
         else:
-            return False
-    elif is_hoop():
-        if answer == "*":
-            return True
-        else:
-            return False
+            if int(answer) == number:
+                return True
+            else:
+                return False
+    except ValueError:
+        answer = input("Wrong! try again: ")
+
+def check_time(current_time):
+    if current_time - start_time > 5:
+        return False
+    else:
+        return True
+
 number = 1
 while True:
-    if computer():
+    if is_computer():
         if is_hoop():
             print("*")
         else:
@@ -35,9 +45,9 @@ while True:
     else:
         start_time = time.time()
         answer = input("")
-        while not True_answer():
+        while not True_answer(answer) and check_time(time.time()):
             answer = input("Wrong! try again: ")
-        if time.time() - start_time > 5:
+        if check_time(time.time()) == False:
             print("Time is over. you lost!")
             break
     number += 1
